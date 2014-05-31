af02a0c3d63cef6b8229e6339c6a994524055636
lalitborse41 <lalitborse41@gmail.com>
2014-05-31 13:10:36 +0530

    Added code
    Added Files...

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', '1vSHJwa9Se9ChzfjBMJvoKntf');
+define('CONSUMER_SECRET', 'C3KbCoeY68E2nXnMFg949lowuuyFW0mHtpSI6ry1ILCZ7TaLVp');
+define('OAUTH_CALLBACK','http://localhost/twitteroauth-master/callback.php');
